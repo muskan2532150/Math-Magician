bd579abf2f3324ec1364b0af240d567838bed4af
muskan2532150 <rg2532150@gmail.com>
2022-09-09 21:15:37 +0530

    Added Class component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,52 +1,56 @@
-import { useState } from 'react';
+import React, { Component } from 'react';
 import calculate from './logic/Calculate';
 
-function Calculator() {
-  const [state, setstate] = useState({ total: '0', next: null, operation: null });
-
-  const clicked = (e) => {
-    const result = calculate({ ...state }, e.target.textContent);
-    setstate(result);
+export default class Calculator extends Component {
+  constructor() {
+    super();
+    this.state = { total: '0', next: null, operation: null };
+  }
+  clicked = (e) => {
+     const result = calculate({ ...this.state }, e.target.textContent);
+      this.setState(result);
+    console.log( );
   };
-  return (
-    <div id="calculate">
+
+  render() {
+    return (
+      <div id="calculate">
       <div>
         <h5>The is Calculator Component</h5>
         <h5>Try it and Enjoy</h5>
       </div>
       <div className="part-two">
         <div className="text-show">
-          {state.total}
+          {this.state.total}
           {' '}
-          {state.operation}
+          {this.state.operation}
           {' '}
-          {state.next}
+          {this.state.next}
           {' '}
         </div>
         <div className="button-con">
-          <div><button type="button" onClick={clicked}>AC</button></div>
-          <div><button type="button" onClick={clicked}>+/-</button></div>
-          <div><button type="button" onClick={clicked}>%</button></div>
-          <div><button type="button" onClick={clicked} className="button-color">÷</button></div>
-          <div><button type="button" onClick={clicked}>7</button></div>
-          <div><button type="button" onClick={clicked}>8</button></div>
-          <div><button type="button" onClick={clicked}>9</button></div>
-          <div><button type="button" onClick={clicked} className="button-color">x</button></div>
-          <div><button type="button" onClick={clicked}>4</button></div>
-          <div><button type="button" onClick={clicked}>5</button></div>
-          <div><button type="button" onClick={clicked}>6</button></div>
-          <div><button type="button" onClick={clicked} className="button-color">-</button></div>
-          <div><button type="button" onClick={clicked}>1</button></div>
-          <div><button type="button" onClick={clicked}>2</button></div>
-          <div><button type="button" onClick={clicked}>3</button></div>
-          <div><button type="button" onClick={clicked} className="button-color">+</button></div>
-          <div><button type="button" onClick={clicked}>0</button></div>
-          <div><button type="button" onClick={clicked}>.</button></div>
-          <div><button type="button" onClick={clicked} className="button-color">=</button></div>
+          <div><button type="button" onClick={this.clicked}>AC</button></div>
+          <div><button type="button" onClick={this.clicked}>+/-</button></div>
+          <div><button type="button" onClick={this.clicked}>%</button></div>
+          <div><button type="button" onClick={this.clicked} className="button-color">÷</button></div>
+          <div><button type="button" onClick={this.clicked}>7</button></div>
+          <div><button type="button" onClick={this.clicked}>8</button></div>
+          <div><button type="button" onClick={this.clicked}>9</button></div>
+          <div><button type="button" onClick={this.clicked} className="button-color">x</button></div>
+          <div><button type="button" onClick={this.clicked}>4</button></div>
+          <div><button type="button" onClick={this.clicked}>5</button></div>
+          <div><button type="button" onClick={this.clicked}>6</button></div>
+          <div><button type="button" onClick={this.clicked} className="button-color">-</button></div>
+          <div><button type="button" onClick={this.clicked}>1</button></div>
+          <div><button type="button" onClick={this.clicked}>2</button></div>
+          <div><button type="button" onClick={this.clicked}>3</button></div>
+          <div><button type="button" onClick={this.clicked} className="button-color">+</button></div>
+          <div><button type="button" onClick={this.clicked}>0</button></div>
+          <div><button type="button" onClick={this.clicked}>.</button></div>
+          <div><button type="button" onClick={this.clicked} className="button-color">=</button></div>
         </div>
       </div>
     </div>
-  );
+    )
+  }
 }
-
-export default Calculator;
