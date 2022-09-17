62ab2933f0b0b76d1ccd90ef51ae531ea0de1c24
techEmprez <choiceosobor@gmail.com>
2022-09-17 12:57:01 +0100

    fix linter errors

--- src/components/calculator.test.js
+++ src/components/calculator.test.js
@@ -1,10 +1,11 @@
-import {render,screen,cleanup} from '@testing-library/react'
-import Calculator from "./Calculator";
+import { render } from '@testing-library/react';
+import React from 'react';
+import Calculator from './Calculator';
 // import dependencies
-import React from 'react'
 // add custom jest matchers from jest-dom
-import '@testing-library/jest-dom'
-it('renders without crashing',()=>{
-    const div = document.createElement('div');
-    render(<Calculator></Calculator>,div);
-})
+import '@testing-library/jest-dom';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  render(<Calculator />, div);
+});
--- src/components/operate.test.js
+++ src/components/operate.test.js
@@ -1,47 +1,48 @@
-import operate from "./logic/operate";
+import operate from './logic/operate';
+
 describe('Addition', () => {
-    // test case: all numbers
-    test('Add 3 to 2', () => {
-      expect(operate(3, 2, '+')).toBe('5');
-    });
-    // test case when one is 0
-    test('Add 3 to 0', () => {
-      expect(operate(3, 0, '+')).toBe('3');
-    });
-    test('Add 3 to -1', () => {
-      expect(operate(3, -1, '+')).toBe('2');
-    });
+  // test case: all numbers
+  test('Add 3 to 2', () => {
+    expect(operate(3, 2, '+')).toBe('5');
   });
-  describe('Subtraction', () => {
-    test('subtract 3 from 2', () => {
-      expect(operate(3, 2, '-')).toBe('1');
-    });
-    test('Subtract 3 from 0', () => {
-      expect(operate(3, 0, '-')).toBe('3');
-    });
-    test('subtract 3 from -1', () => {
-      expect(operate(3, -2, '-')).toBe('5');
-    });
+  // test case when one is 0
+  test('Add 3 to 0', () => {
+    expect(operate(3, 0, '+')).toBe('3');
   });
-  describe('Multiplication', () => {
-    test('Multiply 3 by 2', () => {
-      expect(operate(3, 2, 'x')).toBe('6');
-    });
-    test('multiply 3 by 0', () => {
-      expect(operate(3, 0, 'x')).toBe('0');
-    });
-    test('multiply 3 by -1', () => {
-      expect(operate(3, -1, 'x')).toBe('-3');
-    });
+  test('Add 3 to -1', () => {
+    expect(operate(3, -1, '+')).toBe('2');
   });
-  describe('Division', () => {
-    test('Divide 6 by 2', () => {
-      expect(operate(6, 2, '÷')).toBe('3');
-    });
-    test('divide 6 by 0', () => {
-      expect(operate(6, 0, '÷')).toBe("Can't divide by 0.");
-    });
-    test('divide 6 by -1', () => {
-      expect(operate(6, -1, '÷')).toBe('-6');
-    });
+});
+describe('Subtraction', () => {
+  test('subtract 3 from 2', () => {
+    expect(operate(3, 2, '-')).toBe('1');
   });
+  test('Subtract 3 from 0', () => {
+    expect(operate(3, 0, '-')).toBe('3');
+  });
+  test('subtract 3 from -1', () => {
+    expect(operate(3, -2, '-')).toBe('5');
+  });
+});
+describe('Multiplication', () => {
+  test('Multiply 3 by 2', () => {
+    expect(operate(3, 2, 'x')).toBe('6');
+  });
+  test('multiply 3 by 0', () => {
+    expect(operate(3, 0, 'x')).toBe('0');
+  });
+  test('multiply 3 by -1', () => {
+    expect(operate(3, -1, 'x')).toBe('-3');
+  });
+});
+describe('Division', () => {
+  test('Divide 6 by 2', () => {
+    expect(operate(6, 2, '÷')).toBe('3');
+  });
+  test('divide 6 by 0', () => {
+    expect(operate(6, 0, '÷')).toBe("Can't divide by 0.");
+  });
+  test('divide 6 by -1', () => {
+    expect(operate(6, -1, '÷')).toBe('-6');
+  });
+});
