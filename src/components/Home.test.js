3589e0f6789eefb72fc171e38213eea666f1b9d9
muskan2532150 <rg2532150@gmail.com>
2022-09-17 17:33:29 +0530

    Fixed linter error

--- src/components/Home.test.js
+++ src/components/Home.test.js
@@ -1,12 +1,11 @@
-import {render} from '@testing-library/react'
-import Home from "./Home";
+import { render } from '@testing-library/react';
+import React from 'react';
+import Home from './Home';
 // import dependencies
-import React from 'react'
 // add custom jest matchers from jest-dom
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom';
 
-
-it('renders without crashing for Home',()=>{
-    const div = document.createElement('div');
-    render(<Home></Home>,div);
-})
+it('renders without crashing for Home', () => {
+  const div = document.createElement('div');
+  render(<Home />, div);
+});
--- src/components/Quote.test.js
+++ src/components/Quote.test.js
@@ -1,12 +1,11 @@
-import {render,screen,cleanup} from '@testing-library/react'
-import Quotes from "./Quotes";
+import { render } from '@testing-library/react';
+import React from 'react';
+import Quotes from './Quotes';
 // import dependencies
-import React from 'react'
 // add custom jest matchers from jest-dom
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom';
 
-
-it('renders without crashing for Quotes',()=>{
-    const div = document.createElement('div');
-    render(<Quotes></Quotes>,div);
-})
+it('renders without crashing for Quotes', () => {
+  const div = document.createElement('div');
+  render(<Quotes />, div);
+});
